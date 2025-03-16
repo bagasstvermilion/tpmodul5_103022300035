490ec917d6fc52217a7f94867e3c19387fe1422a
Bagas Pratama <bagassss.com@gmail.com>
2025-03-16 23:44:45 +0700

    Menambahkan class HaloGeneric dengan method SapaUser

--- tpmodul5_103022300035/Program.cs
+++ tpmodul5_103022300035/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using tpmodul5_103022300035;
+
+class Program
+{
+    static void Main()
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Bagas");
+    }
+}
